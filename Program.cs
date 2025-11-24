8dfcd05e70090810344b3e0480420a62d284d0bf
huangwenwu <jikhww@163.com>
2025-11-24 16:43:31 +0800

    ssl error

--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Net;
+
+ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls13;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
