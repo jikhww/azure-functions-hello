d903c6d09cfdc9d6f2c8a97ec520f3b31ea4dd75
huangwenwu <jikhww@163.com>
2025-11-24 17:25:35 +0800

    ssl error

--- Program.cs
+++ Program.cs
@@ -2,6 +2,12 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Net;
+using System.Net.Security;
+
+ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
+//这里设置了协议类型。
+ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
