3572bd3fedd8f425aea548accea445232f8509b0
huangwenwu <jikhww@163.com>
2025-11-24 17:50:21 +0800

    ssl error

--- HttpTrigger.cs
+++ HttpTrigger.cs
@@ -1,8 +1,11 @@
+using Kingdee.CDP.WebApi.SDK;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using System.Net;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 namespace Kingdee.Function;
@@ -38,4 +41,49 @@ public class HttpTrigger
         }
     }
 
+}
+
+public static class KingdeePatch
+{
+
+    public static void Patch()
+    {
+        MethodInfo? originalMethod = typeof(WebRequestHelper)
+            .GetMethod("SetSecurityProtocol", BindingFlags.NonPublic | BindingFlags.Static);
+        MethodInfo? replacementMethod = typeof(KingdeePatch)
+           .GetMethod("CustomSetSecurityProtocol", BindingFlags.NonPublic | BindingFlags.Static);
+        if(originalMethod == null || replacementMethod == null)
+        {
+            Console.WriteLine("Method not found for patching.");
+            return;
+        }
+        ReplaceMethod(originalMethod, replacementMethod);
+    }
+
+    private static void CustomSetSecurityProtocol(Uri uri)
+    {
+    }
+    private static unsafe void ReplaceMethod(MethodInfo original, MethodInfo replacement)
+    {
+        if (original == null || replacement == null)
+        {
+            throw new ArgumentNullException("Methods cannot be null.");
+        }
+
+        if (original.MethodHandle.Value == IntPtr.Zero || replacement.MethodHandle.Value == IntPtr.Zero)
+        {
+            throw new InvalidOperationException("Method handles must be valid.");
+        }
+
+        // 准备方法以确保它们的指针可用
+        RuntimeHelpers.PrepareMethod(original.MethodHandle);
+        RuntimeHelpers.PrepareMethod(replacement.MethodHandle);
+
+        // 获取方法指针
+        IntPtr* originalMethodPtr = (IntPtr*)original.MethodHandle.Value.ToPointer() + 1;
+        IntPtr* replacementMethodPtr = (IntPtr*)replacement.MethodHandle.Value.ToPointer() + 1;
+
+        // 替换方法指针
+        *originalMethodPtr = *replacementMethodPtr;
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,14 +1,11 @@
+using Kingdee.Function;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Net;
 using System.Net.Security;
-
-ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
-//这里设置了协议类型。
-ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
-
+KingdeePatch.Patch();
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
